3c9fda7122e53e63feb750a54d6ff36c8246010f
Colin Benson <colin.j.benson@gmail.com>
2020-11-20 17:37:56 -0500

    Added scriptable object base

---
+++ Assets/Scripts/WeaponScripts/WeaponTypes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MetroidVaniaTools
+{
+    [CreateAssetMenu(fileName = "WeaponType", menuName = "Metroidvania/Weapons", order= 1)]
+    public class WeaponTypes : ScriptableObject
+    {
+        public GameObject projectile;
+        public float projectileSpeed;
+
+    }
+}
