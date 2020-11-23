de25af5d5807fdbc57b963578bd031207ff0d09a
Colin Benson <colin.j.benson@gmail.com>
2020-11-23 01:07:11 -0500

    We've got doors to next scene prefabs

---
+++ Assets/Scripts/LevelScripts/Managers/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace MetroidVaniaTools
+{
+    public class Door : Managers
+    {
+        [SerializeField]
+        protected string[] tagsToOpen;
+
+        protected virtual void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (tagsToOpen.Contains(collision.gameObject.tag))  
+            {
+                GetComponent<Collider2D>().enabled = false;
+                GetComponent<Animator>().SetBool("Open", true);
+            }
+
+        }
+    }
+}
