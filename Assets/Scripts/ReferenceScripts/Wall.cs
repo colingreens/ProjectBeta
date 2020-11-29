cdc83eb7dae183dfa732218e3d8610b8bc21b5d9
Colin Benson <colin.j.benson@gmail.com>
2020-11-29 02:02:29 -0500

    Destructable wall seems cool

--- Assets/Scripts/ReferenceScripts/Wall.cs
+++ Assets/Scripts/ReferenceScripts/Wall.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
-namespace Assets.Scripts.ReferenceScripts
+namespace MetroidVaniaTools
 {
-    class Wall
+    public class Wall : MonoBehaviour
     {
+        [SerializeField]
+        private AudioClip chopSound1;
+        private AudioClip chopSound2;
+        public Sprite dmgSprite;
+        public int hp = 3;
+        private SpriteRenderer spriteRenderer;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        public void DamageWall(int loss)
+        {
+            //SoundManager.instance.RandomizeSfx(chopSound1, chopSound2);
+
+            spriteRenderer.sprite = dmgSprite;       
+            hp -= loss;
+            if (hp <= 0)
+                gameObject.SetActive(false);
+        }
     }
 }
