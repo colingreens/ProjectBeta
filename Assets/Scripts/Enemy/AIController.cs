f183c6a4b29bcb3a5ed3e339b56763093bfbdfb8
Colin Benson <colin.j.benson@gmail.com>
2020-11-29 01:01:54 -0500

    AI Controller start and meta files

---
+++ Assets/Scripts/Enemy/AIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MetroidVaniaTools
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField]
+        private MovementConfig _movementConfig;
+        [SerializeField]
+        private JumpConfig jump;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
