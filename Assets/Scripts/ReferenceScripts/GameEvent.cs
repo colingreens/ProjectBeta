bb97bcf6e77e24fd68182b99b93eb852170df4a2
Colin Benson <colin.j.benson@gmail.com>
2020-12-01 21:55:45 -0500

    added references for scriptable objects and events

---
+++ Assets/Scripts/ReferenceScripts/GameEvent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MetroidVaniaTools
+{
+    [CreateAssetMenu]
+    public class GameEvent : ScriptableObject
+    {
+        private List<GameEventListener> listeners =
+            new List<GameEventListener>();
+
+        public void Raise()
+        {
+            foreach (var listener in listeners)
+            {
+                listener.OnEventRaised();
+            }
+        }
+
+        public void RegisterListener(GameEventListener listener)
+        {
+            listeners.Remove(listener);
+        }
+
+        public void UnregisterListener(GameEventListener listener)
+        {
+            listeners.Remove(listener);
+        }
+    }
+}
---
+++ Assets/Scripts/ReferenceScripts/GameEventListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MetroidVaniaTools
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        public GameEvent Event;
+        public UnityEvent Response;
+
+        private void OnEnable()
+        {
+            Event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            Event.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            Response.Invoke();
+        }
+    }
+}
