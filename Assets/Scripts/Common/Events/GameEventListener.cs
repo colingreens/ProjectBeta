780cb0a101c49124956835d33095ab4d6b99f7e1
Colin Benson <colin.j.benson@gmail.com>
2020-12-05 01:39:09 -0500

    Create GameEventListener.cs

---
+++ Assets/Scripts/Common/Events/GameEventListener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace MetroidVaniaTools
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        public GameEvent Event;
+        public UnityEvent Response;
+
+        private void OnEnable()
+        {
+            Event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            Event.UnregisterListener(this);
+        }
+
+        public void RaiseEvent()
+        {
+            Response.Invoke();
+        }
+    }
+}
