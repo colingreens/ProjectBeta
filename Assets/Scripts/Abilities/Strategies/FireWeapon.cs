8883a9890b73c55767e41cce94df49ec5ae77481
Colin Benson <colin.j.benson@gmail.com>
2020-12-20 11:54:55 -0500

    Abstracted strategies

---
+++ Assets/Scripts/Abilities/Strategies/FireWeapon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace MetroidVaniaTools
+{
+    [CreateAssetMenu(fileName = "FireCommand", menuName = "Strategy/FireCommand", order = 1)]
+    public class FireWeapon : Strategy
+    {
+        public GameObject Projectile;
+        public float Velocity;
+        
+        public override void Execute(Character character)
+        {
+            FireTheWeapon(character.MovementDirection, character.transform.position);
+        }
+
+        private void FireTheWeapon(float directionFacing, Vector3 position)
+        {
+            var bulletActive = Instantiate(Projectile);
+            bulletActive.transform.position = position;
+            if (directionFacing < 0)
+            {
+                bulletActive.transform.rotation = Quaternion.Euler(180, 0, 180);
+            }
+            bulletActive.GetComponent<Rigidbody2D>().velocity = new Vector2(directionFacing * Velocity , 0f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Abilities/Strategies/Strategies.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MetroidVaniaTools
+{
+    public abstract class Strategy : ScriptableObject, IStrategy
+    {
+        public abstract void Execute(Character character);
+    }
+
+    public interface IStrategy
+    {
+        void Execute(Character character);
+    }
+
+}
